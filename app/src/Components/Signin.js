3e8bd560860774b00ec92e72a88e866663d80ebf
Deborah Warren <Deborah.dw.32@gmail.com>
2021-05-19 18:23:15 -0400

    barebones of sign in form

---
+++ app/src/Components/Signin.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const initialFormValues = {
+    username: '',
+    password: '',
+}
+
+const initialFormErrors = {
+    username: '',
+    password: '',
+}
+
+const Signin = () => {
+    const [users, setUsers] = useState([])
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formErrors, setFormErrors] = useState(inistialFormErrors)
+    const [disabled, setDisabled] = useState(true)
+
+    return(
+        <div>
+            <h3>Sign In</h3>
+            <p>New to Water My Plants? Sign up to get started here!</p>
+            {/* Link to sign up page here */}
+            <form>
+                <label>Username
+                    <input
+                        type='text'
+                        value={formValues.username}
+                    />
+                </label>
+                <label>Password
+                    <input
+                        type='password'
+                        value={formValues.password}
+                    />
+                </label>
+                <button disabled={disabled}>Sign In</button>
+            </form>
+        </div>
+    )
+}
