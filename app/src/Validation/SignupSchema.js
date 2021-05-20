71517e9157c98d02946115bfe6d202dae4c15c34
Deborah Warren <Deborah.dw.32@gmail.com>
2021-05-19 21:11:19 -0400

    yup validation for sign up form

--- app/src/Validation/SignupSchema.js
+++ app/src/Validation/SignupSchema.js
@@ -6,4 +6,4 @@ const signUpSchema = yup.object().shape({
     password: yup.string().min(8, 'Password must be at least 8 characters!').required('Password is required!')
 })
 
-export default signUpSchema
+export default signUpSchema;
