83987947f942c5497dae5befa9b88ac22a592b9f
Deborah Warren <Deborah.dw.32@gmail.com>
2021-05-19 20:06:03 -0400

    added default export to sign up validation

--- app/src/Validation/SignupSchema.js
+++ app/src/Validation/SignupSchema.js
@@ -5,3 +5,5 @@ const signUpSchema = yup.object().shape({
     email: yup.string().email('Must be a valid email!').required('Email is required!'),
     password: yup.string().min(8, 'Password must be at least 8 characters!').required('Password is required!')
 })
+
+export default signUpSchema
