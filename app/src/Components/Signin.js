63479cbe55b695886b314c6fa5865962c43a4b53
Deborah Warren <Deborah.dw.32@gmail.com>
2021-05-19 21:04:09 -0400

    added validations and input change to sign in form

--- app/src/Components/Signin.js
+++ app/src/Components/Signin.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import signInSchema from '../Validation/SignInSchema' 
+import * as yup from 'yup'
 
 const initialFormValues = {
     username: '',
@@ -16,6 +18,37 @@ const Signin = () => {
     const [formErrors, setFormErrors] = useState(inistialFormErrors)
     const [disabled, setDisabled] = useState(true)
 
+    const onInputChange =  event => {
+        const { name, value } = event.target
+
+        yup.reach(signInSchema, name)
+        .validate(value)
+        .then(() => {
+            setFormErrors({...formErrors, [name]: ''})
+        })
+        .catch(error => {
+            setFormErrors({...formErrors, [name]: error.errors[0]})
+        })
+
+        setFormValues({...formValues, [name]: value})
+    }
+
+    const onSubmit = event =>{
+        event.preventDefault()
+
+        const existingUser = {
+            username: formValues.username.trim(),
+            password: formValues.password.trim(),
+        }
+        console.log('Existing user sign in', existingUser)
+    }
+
+    useEffect(() =>{
+        signInSchema.isValid(formValues).then(valid => {
+            setDisabled(!valid)
+        })
+    }, [formValues])
+
     return(
         <div>
             <h3>Sign In</h3>
@@ -26,14 +59,22 @@ const Signin = () => {
                     <input
                         type='text'
                         value={formValues.username}
+                        name='username'
+                        onChange={onInputChange}
                     />
                 </label>
                 <label>Password
                     <input
                         type='password'
                         value={formValues.password}
+                        name='password'
+                        onChange={onInputChange}
                     />
                 </label>
+                <div className='errors'>
+                    <div>{formErrors.username}</div>
+                    <div>{formErrors.password}</div>
+                </div>
                 <button disabled={disabled}>Sign In</button>
             </form>
         </div>
