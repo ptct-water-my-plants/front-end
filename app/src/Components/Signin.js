9e627df687bac9a0298039d14c1fc7f073871d8a
Deborah Warren <Deborah.dw.32@gmail.com>
2021-05-20 21:46:10 -0400

    input changes and onSubmits added to both forms

--- app/src/Components/Signin.js
+++ app/src/Components/Signin.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import signInSchema from "../Validation/SigninSchema";
+import * as yup from "yup";
+import { Link } from "react-router-dom";
 
 const initialFormValues = {
   username: "",
@@ -11,26 +14,72 @@ const initialFormErrors = {
 };
 
 const Signin = () => {
-  const [users, setUsers] = useState([]);
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const [formErrors, setFormErrors] = useState(initialFormErrors);
-  const [disabled, setDisabled] = useState(true);
+    // const [users, setUsers] = useState([]);
+    const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState(initialFormErrors);
+    const [disabled, setDisabled] = useState(true);
 
-  return (
-    <div>
-      <h3>Sign In</h3>
-      <p>New to Water My Plants? Sign up to get started here!</p>
-      {/* Link to sign up page here */}
-      <form>
-        <label>
-          Username
-          <input type="text" value={formValues.username} />
-        </label>
-        <label>
-          Password
-          <input type="password" value={formValues.password} />
-        </label>
-        <button disabled={disabled}>Sign In</button>
+    const onInputChange =  event => {
+        const { name, value } = event.target
+
+        yup.reach(signInSchema, name)
+        .validate(value)
+        .then(() => {
+            setFormErrors({...formErrors, [name]: ''})
+        })
+        .catch(error => {
+            setFormErrors({...formErrors, [name]: error.errors[0]})
+        })
+
+        setFormValues({...formValues, [name]: value})
+    }
+
+    const onSubmit = event =>{
+        event.preventDefault()
+
+        const existingUser = {
+            username: formValues.username.trim(),
+            password: formValues.password.trim(),
+        }
+        console.log('Existing user sign in', existingUser)
+    }
+
+    useEffect(() => {
+        signInSchema.isValid(formValues).then((valid) => {
+         setDisabled(!valid)
+        })
+     }, [formValues])  
+
+    return (
+        <div>
+            <h3>Sign In</h3>
+            <p>New to Water My Plants? 
+                <Link to={`/`}>
+                    <span>Sign up to get started here!</span>
+                </Link>
+            </p>
+            <form onSubmit={onSubmit}>
+                <label>Username
+                    <input
+                        type='text'
+                        value={formValues.username}
+                        name='username'
+                        onChange={onInputChange}
+                    />
+                </label>
+                <label>Password
+                    <input
+                        type='password'
+                        value={formValues.password}
+                        name='password'
+                        onChange={onInputChange}
+                    />
+                </label>
+            <button disabled={disabled}>Sign In</button>
+            <div className='errors'>
+                <div>{formErrors.username}</div>
+                <div>{formErrors.password}</div>
+            </div>
       </form>
     </div>
   );
--- app/src/Components/Signup.js
+++ app/src/Components/Signup.js
@@ -1,45 +1,108 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import signUpSchema from "../Validation/SignupSchema";
+import * as yup from "yup";
+import { Link } from "react-router-dom";
 
 const initialFormValues = {
-  username: "",
-  email: "",
-  password: "",
+    username: "",
+    email: "",
+    password: "",
 };
 
-const inistialFormErrors = {
-  username: "",
-  email: "",
-  password: "",
+const initialFormErrors = {
+    username: "",
+    email: "",
+    password: "",
 };
 
 const Signup = () => {
-  const [users, setUsers] = useState([]);
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const [formErrors, setFormErrors] = useState(inistialFormErrors);
-  const [disabled, setDisabled] = useState(true);
+    const [users, setUsers] = useState([])
+    const [formValues, setFormValues] = useState(initialFormValues)
+    const [formErrors, setFormErrors] = useState(initialFormErrors)
+    const [disabled, setDisabled] = useState(true)
 
-  return (
-    <div>
-      <h3>Sign Up for an Account</h3>
-      <p>Already have an account? Sign in here!</p>
-      {/* Link to sign in page here  */}
-      <form>
-        <label>
-          Username
-          <input type="text" value={formValues.username} />
-        </label>
-        <label>
-          Email
-          <input type="email" value={formValues.email} />
-        </label>
-        <label>
-          Password
-          <input type="password" value={formValues.email} />
-        </label>
-        <button disabled={disabled}>Sign Up</button>
-      </form>
-    </div>
-  );
-};
+    const postUser = newUser => {
+        setUsers([...users, newUser])
+        console.log('User Signup', users)
+
+        setFormValues(initialFormValues)
+    }
+
+    const onInputChange = event =>{
+        const { name, value } = event.target
+
+        yup.reach(signUpSchema, name)
+        .validate(value)
+        .then(() => {
+            setFormErrors({...formErrors, [name]: ''})
+        })
+
+        .catch(error => {
+            setFormErrors({...formErrors, [name]: error.errors[0]})
+        })
+
+        setFormValues({...formValues, [name]: value})
+    }
+
+    const onSubmit = event => {
+        event.preventDefault()
+
+        const newUser = {
+            username: formValues.username.trim(),
+            email: formValues.email.trim(),
+            password: formValues.password.trim(),
+        }
+        postUser(newUser)
+    }
+
+   useEffect(() => {
+       signUpSchema.isValid(formValues).then((valid) => {
+        setDisabled(!valid)
+       })
+    }, [formValues])   
+
+    return(
+        <div>
+            <h3>Sign Up for an Account</h3>
+            <p>Already have an account?
+                <Link to={`/signin`}>
+                    <span>Sign in here!</span>
+                </Link>
+            </p>
+            <form onSubmit={onSubmit}>
+            <label>Username 
+                    <input
+                        type='text'
+                        value={formValues.username}
+                        name='username'
+                        onChange={onInputChange}
+                    />
+                </label>
+                <label>Email
+                    <input
+                        type='email'
+                        value={formValues.email}
+                        name='email'
+                        onChange={onInputChange}
+                    />
+                </label>
+                <label>Password
+                    <input
+                        type='password'
+                        value={formValues.email}
+                        name='password'
+                        onChange={onInputChange}
+                    />
+                </label>
+                <button disabled={disabled}>Sign Up</button>
+                <div className='errors'>
+                    <div>{formErrors.username}</div>
+                    <div>{formErrors.email}</div>
+                    <div>{formErrors.password}</div>
+                </div>
+            </form>
+        </div>
+    )
+}
 
 export default Signup;
