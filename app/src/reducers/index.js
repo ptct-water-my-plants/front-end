72ad1e333829338a2355e52a411e42936515723e
Ethan Karp <ethansmainemail@gmail.com>
2021-05-23 21:58:41 -0600

    connected Signup component to redux

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -14,7 +14,6 @@ export const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  console.log("reducer", action);
   switch (action.type) {
     case 'SET_USERS':
       return {
